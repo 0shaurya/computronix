dc8f43f03775a65404f1a6c3527b4e732b8413ae
Shivam Vashishtha <58797316+ShivamVashishtha@users.noreply.github.com>
2022-09-02 22:00:35 -0500

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
 img = 1000000
-imgs = ["https://www.computronix.co/images/slideacer10.jpg",
-"https://www.computronix.co/images/slideacer11.jpg",
-"https://www.computronix.co/images/slideacer13.jpg"]
+imgs = ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+"https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1520x800.png",
+"https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q="]
 
 document.querySelector(".left-btn").addEventListener("click", move(-1))
 document.querySelector(".left-btn").addEventListener("click", move(1))
